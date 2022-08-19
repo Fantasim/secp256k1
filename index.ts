53106b1aa61cc39090623585e562164f2ab87283
fantasim <siuol@gmx.com>
2022-08-19 07:36:07 +0300

    privk

--- index.ts
+++ index.ts
@@ -20,3 +20,63 @@ const CURVE = Object.freeze({
   Gx: BigInt('55066263022277343669578718895168534326250603453777594175500187360389116729240'),
   Gy: BigInt('32670510020758816978083085130507043184471273380659243275938904335757337482424'),
 });
+
+class ModMath {
+
+  static mod = (a: bigint, b = CURVE.P) => {
+    const result = a % b
+    return result >= 0 ? result : result + b
+  }
+
+  static invert = (a: bigint, b: bigint): bigint => {
+    if (a === 0n || b <= _0n){
+      throw new Error(`invert: expected positive integers, got n=${a} mod=${b}`);
+    }
+
+    let r1 = b
+    let r2 = a
+    let t1 = _0n
+    let t2 = _1n
+    let t: bigint
+    let r: bigint
+
+    while (r2 > 0){
+      let q = r1 / r2
+      r = r1 % r2
+      t = t1 - (q * t2)
+
+      r1 = r2
+      r2 = r
+      t1 = t2
+      t2 = t
+    }
+    if (r1 !== _1n){
+      throw new Error(`invert doesn't not exist on ${a.toString()}`)
+    }
+    return ModMath.mod(t1, b)
+  }
+}
+
+const hexToNumber = (hex: string) => BigInt(`0x${hex}`)
+const bytesToHex = (b: Buffer) => b.toString('hex')
+
+class PrivateKey {
+
+  static random = () => {
+    const ret: number[] = []
+    for (let i = 0; i < 32; i++){
+      ret.push(Math.random() * 255)
+    }
+    return new PrivateKey(Buffer.from(ret))
+  }
+
+  private _p: bigint = _0n
+  constructor(p: bigint | Buffer){
+    if (p instanceof Buffer) {
+        this._p = ModMath.mod(hexToNumber(bytesToHex(p)))
+    } else {
+      this._p = ModMath.mod(p)
+    }
+  }
+  get = () => this._p
+}
